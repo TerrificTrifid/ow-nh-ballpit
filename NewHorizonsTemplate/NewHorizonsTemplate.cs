0acd7acaf7c860bbac1448b7e125cdb7a6053906
TerrificTrifid <99054745+TerrificTrifid@users.noreply.github.com>
2023-03-25 21:02:22 -0500

    Initial commit

---
+++ NewHorizonsTemplate/NewHorizonsTemplate.cs
@@ -0,0 +1,24 @@
+﻿using OWML.Common;
+using OWML.ModHelper;
+using OWML.Utils;
+using UnityEngine;
+using UnityEngine.PostProcessing;
+using UnityEngine.SceneManagement;
+using UnityEngine.InputSystem;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NewHorizonsTemplate
+{
+    public class NewHorizonsTemplate : ModBehaviour
+    {
+        public INewHorizons NewHorizonsAPI;
+
+        private void Start()
+        {
+            NewHorizonsAPI = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            NewHorizonsAPI.LoadConfigs(this);
+        }
+    }
+}
